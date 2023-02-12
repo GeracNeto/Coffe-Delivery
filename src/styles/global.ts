d4587cbf6029cefbd360b1422e6bf77fa7385b79
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-02-12 18:22:06 -0300

    style(global): remove ul style, add webkit-font to body

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,7 +5,16 @@ export const GlobalStyle = createGlobalStyle`
     margin:0;
     padding:0;
     box-sizing: border-box;
-}`;
+}
+
+ul{
+    list-style: none;
+}
+
+body{
+    -webkit-font-smoothing: antialiased;
+}
+`;
 
 /*
 font-family: 'Baloo 2', cursive;
