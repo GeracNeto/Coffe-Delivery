60d6dca18cbd4ecde66602059c83118180aeb0b0
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-02-12 15:55:31 -0300

    update(App): update App with Header component and Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from "styled-components";
+import { Header } from "./components/Header";
+import { Home } from "./pages/Home";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Coffee Delivery</h1>
+      <Header />
+      <Home />
       <GlobalStyle />
     </ThemeProvider>
   );
