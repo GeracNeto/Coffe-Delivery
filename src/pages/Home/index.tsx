b8af8e1575f263c3ba6298babb61f1a48ce10be5
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-02-12 18:22:41 -0300

    feat(Home): add Home page elements

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,52 @@
-import { MainStyled } from "./styles";
+import { List, MainStyled, SectionStyled, Slogan } from "./styles";
+
+import coffee from "../../assets/coffee.svg";
+import cartIcon from "../../assets/cart_icon.svg";
+import coffeeIcon from "../../assets/coffee_icon.svg";
+import packageIcon from "../../assets/package_icon.svg";
+import timeIcon from "../../assets/time_icon.svg";
+
+import { Header } from "../../components/Header";
 
 export function Home() {
-  return <MainStyled></MainStyled>;
+  return (
+    <MainStyled>
+      <Header />
+      <SectionStyled>
+        <div>
+          <Slogan>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <h2>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </h2>
+          </Slogan>
+          <List>
+            <div>
+              <li>
+                <img src={cartIcon} alt="cart icon" />
+                Compra simples e segura
+              </li>
+              <li>
+                <img src={timeIcon} alt="time icon" />
+                Entrega rápida e rastreada
+              </li>
+            </div>
+            <div>
+              <li>
+                <img src={packageIcon} alt="package icon" />
+                Embalagem mantém o café intacto
+              </li>
+              <li>
+                <img src={coffeeIcon} alt="coffee icon" />O café chega
+                fresquinho até você
+              </li>
+            </div>
+          </List>
+        </div>
+        <img src={coffee} alt="coffe logo" />
+      </SectionStyled>
+      <main>main</main>
+    </MainStyled>
+  );
 }
