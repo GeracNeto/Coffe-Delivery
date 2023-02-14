c187f8f305e33c7a6ffbccd83ab615b52a77da70
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-02-13 22:04:10 -0300

    style(CartCatalog): create styles

---
+++ src/components/Buttons/CartCatalog/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const CartCatalogStyled = styled.div`
+  padding: 0.5rem;
+  border-radius: 6px;
+  background: ${(props) => props.theme.product["purple-dark"]};
+`;
