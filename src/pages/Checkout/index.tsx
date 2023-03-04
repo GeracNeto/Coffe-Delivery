98970396a46568d24ef8ed94c84bb5badde1e361
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-03-03 21:57:08 -0300

    feat(Checkout): add andress form

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,6 +1,9 @@
+import { MapPinLine } from "phosphor-react";
 import {
+  AdressForm,
   BottomtContainer,
   CheckoutStyled,
+  InputContainer,
   LeftContainer,
   RightContainer,
   TopContainer,
@@ -10,7 +13,29 @@ export function Checkout() {
   return (
     <CheckoutStyled>
       <LeftContainer>
-        <TopContainer>top</TopContainer>
+        <TopContainer>
+          <header>
+            <MapPinLine size={22} weight="thin" color="#C47F17" />
+            <div>
+              <span>Endereço de Entrega</span>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </header>
+
+          <AdressForm>
+            <InputContainer placeholder="CEP" variant="cep" />
+            <InputContainer placeholder="Rua" variant="rua" />
+            <div>
+              <InputContainer placeholder="Número" variant="numero" />
+              <InputContainer placeholder="Complemento" variant="complemento" />
+            </div>
+            <div>
+              <InputContainer placeholder="Bairro" variant="bairro" />
+              <InputContainer placeholder="Cidade" variant="cidade" />
+              <InputContainer placeholder="UF" variant="uf" />
+            </div>
+          </AdressForm>
+        </TopContainer>
         <BottomtContainer>bottom</BottomtContainer>
       </LeftContainer>
       <RightContainer>right</RightContainer>
