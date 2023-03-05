09f006756021c81f2e3559b6d49b55339088e6cf
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-03-05 16:47:57 -0300

    style(Success): create inital styles

---
+++ src/pages/Success/style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const SuccessContainer = styled.div`
+  padding: 5.75rem 10rem;
+  margin: 0 auto;
+  max-width: 1440px;
+`;
+
+export const InfoContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+
+  div {
+    border: 1px solid;
+    border-radius: 6px 36px;
+    border-image: linear-gradient(to right, #dbac2c, #8047f8) 1;
+    width: 50%;
+  }
+`;
