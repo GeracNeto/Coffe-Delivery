65ac75d929ab07d68a625d5b29f46ea0f0f40fe1
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-03-06 20:12:27 -0300

    feat(Success): create deliver info container

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,6 +1,14 @@
-import { InfoContainer, SuccessContainer } from "./style";
+import {
+  InfoContainer,
+  LastInfo,
+  LastInfosContainer,
+  SuccessContainer,
+} from "./style";
 
 import illustration from "../../assets/illustration.svg";
+import location from "../../assets/location.svg";
+import timer from "../../assets/time_icon.svg";
+import money from "../../assets/iconmoney.svg";
 
 export function Success() {
   return (
@@ -10,7 +18,32 @@ export function Success() {
         <p>Agora é só aguardar que logo o café chegará até você</p>
       </header>
       <InfoContainer>
-        <div>content</div>
+        <LastInfosContainer>
+          <LastInfo>
+            <img src={location} alt="location-icon" />
+            <div>
+              <p>
+                Entrega em
+                <span> Rua João Daniel Martinelli, 102</span>
+              </p>
+              <p>Farrapos - Porto Alegre, RS</p>
+            </div>
+          </LastInfo>
+          <LastInfo>
+            <img src={timer} alt="location-icon" />
+            <div>
+              <p>Previsão de enrtega</p>
+              <span>20 min - 30 min</span>
+            </div>
+          </LastInfo>
+          <LastInfo>
+            <img src={money} alt="location-icon" />
+            <div>
+              <p>Pagamento na entrega</p>
+              <span>Cartão de Crédito</span>
+            </div>
+          </LastInfo>
+        </LastInfosContainer>
         <img src={illustration} alt="illustration" />
       </InfoContainer>
     </SuccessContainer>
