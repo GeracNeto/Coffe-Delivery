bd4e342429375df82e640a849da94a42e960af61
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-03-18 15:15:35 -0300

    feat(CountCart): creates a hook that return cart quantity

---
+++ src/hooks/useCountCart.ts
@@ -0,0 +1,25 @@
+import { useContext, useEffect, useState } from "react";
+import { CoffeesLitsContext } from "../contexts/CoffeesLitsContext";
+
+export function useCountCart() {
+  const [count, setCount] = useState({ quantity: 0 });
+  const { cart } = useContext(CoffeesLitsContext);
+
+  useEffect(() => {
+    const quantityOfItems = cart.reduce(
+      (acc, items) => {
+        acc.quantity += items.quantity;
+
+        return acc;
+      },
+
+      {
+        quantity: 0,
+      }
+    );
+
+    setCount(quantityOfItems);
+  }, [cart]);
+
+  return count.quantity;
+}
